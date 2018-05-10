60e1c15d7b7e183ed58de0115ee9e8a52102b5b3
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2018-05-09 23:40:16 -0400

    form is working

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,44 @@
-const ohHey = "Hello World";
+// console.log("hello");
 
-console.log(ohHey);
+$(function() {
+  var $form = $('form#test-form'),
+      url = 'https://script.google.com/macros/s/AKfycbyVLkgTg5hOLlUM0Qg0gJr31VIerLahzIck7dYsBo34X5-HY7l3/exec'
+
+//   $('#submit-form').on('click', function (e) {
+//     e.preventDefault();
+//     console.log("lalal");
+//     var jqxhr = $.ajax({
+//       url: url,
+//       method: "GET",
+//       dataType: "json",
+//       data: form.serializeObject()
+//     }).success(
+//       // do something
+//     );
+//   })
+// });
+
+// HANDLE FORM SUBMIT
+// https://script.google.com/macros/u/1/s/AKfycbwa4fAR5TN0qIpF06evCkhPrNFLb4LuiiAJEpQn5VWariKIvCE/exec
+
+// var $form = $('form#test-form'),
+//   url = 'https://script.google.com/macros/s/AKfycbwa4fAR5TN0qIpF06evCkhPrNFLb4LuiiAJEpQn5VWariKIvCE/exec'
+
+$('form').on('submit', function (e) {
+  e.preventDefault();
+  // addActive();
+  console.log(this);
+  var form = $(this).serializeArray();
+  const url = 'https://script.google.com/macros/s/AKfycbyVLkgTg5hOLlUM0Qg0gJr31VIerLahzIck7dYsBo34X5-HY7l3/exec'
+  console.log(form);
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: form
+  }).then((data) => {
+    console.log(data);
+  });
+});
+
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,24 @@
-"use strict";
+// 'use strict';
 
-var ohHey = "Hello World";
+// console.log("hello");
 
-console.log(ohHey);
+$(function () {
+
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    // addActive();
+    console.log(this);
+    var form = $(this).serializeArray();
+    // console.log(form);
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbwLn6XIo1taoILeDZUqwbzMdqA2EuzGUe4UsN2ntha70u3xO5UG/exec',
+      method: "GET",
+      dataType: "json",
+      data: form
+    }).then(function(data){
+      console.log(data)
+    });
+  });
+});
+
+// serializeObject()
