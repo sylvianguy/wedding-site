0607b2d5a130f34a233708ec37dc0fcaff7ffbb8
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2018-05-15 00:10:06 -0400

    styling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,24 +1,28 @@
 'use strict';
 
-// 'use strict';
-
-// console.log("hello");
-
 $(function () {
 
   $('form').on('submit', function (e) {
     e.preventDefault();
-    // addActive();
-    console.log(this);
     var form = $(this).serializeArray();
-    // console.log(form);
     $.ajax({
       url: 'https://script.google.com/macros/s/AKfycbwLn6XIo1taoILeDZUqwbzMdqA2EuzGUe4UsN2ntha70u3xO5UG/exec',
       method: "GET",
       dataType: "json",
       data: form
     }).then(function (data) {
-      console.log(data);
+      var res = data.result;
+
+      if (res === 'success') {
+        $('.modal').addClass('show');
+      }
+
+      $('#close--modal').on('click', function (e) {
+        e.preventDefault();
+        $('.modal').removeClass('show');
+        $('input[type="text"], textarea').val('');
+        $('select').prop('selectedIndex', 0);
+      });
     });
   });
 });
