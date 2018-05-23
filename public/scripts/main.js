0fb1f533ffca10273e92d652386dd91f09acdab9
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2018-05-22 21:40:03 -0400

    timer working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,9 @@
 
 $(function () {
 
+  // const daysFromNow = moment().add(135, 'days').calendar(); 
+  // console.log(daysFromNow)
+
   $('form').on('submit', function (e) {
     e.preventDefault();
     var form = $(this).serializeArray();
@@ -25,6 +28,47 @@ $(function () {
       });
     });
   });
+
+  function countDown() {
+    var currentDate = new Date();
+    console.log(currentDate);
+    var endDate = new Date('September 28, 2018');
+    var timeDiff = endDate - currentDate;
+
+    timeDiff = Math.round(timeDiff / 1000);
+    // calculate (and subtract) whole hours
+    var hours = Math.floor(timeDiff / 3600);
+    var days = Math.floor(hours / 24);
+
+    var newHours = hours % 24;
+
+    timeDiff -= hours * 3600;
+
+    // if (hours < 10) { hours = "0" + hours; }
+    // if (minutes < 10) { minutes = "0" + minutes; }
+    // if (seconds < 10) { seconds = "0" + seconds; }
+
+    // calculate (and subtract) whole minutes
+    var minutes = Math.floor(timeDiff / 60) % 60;
+
+    timeDiff -= minutes * 60;
+    var seconds = timeDiff % 60;
+
+    if (currentDate <= endDate) {
+      $('.days').html(days);
+      $('.hours').html(newHours);
+      $('.mins').html(minutes);
+      $('.secs').html(seconds);
+      return;
+    } else {
+      console.log("weeeee get married");
+      clearInterval(interval);
+    }
+  }
+
+  var interval = setInterval(function () {
+    countDown();
+  }, 1000);
 });
 
 // serializeObject()
